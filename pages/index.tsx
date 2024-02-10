fa3b8eb7c593dfa07220a1eaa2ad668412bd1457
Bahram Movlanov <bmovlanov@gmail.com>
2024-02-09 20:39:55 -0500

    added gradient background component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
+import { GradientBackground } from '@/components/QuoteGenerator/QuoteGeneratorElements';
 
 export default function Home() {
   return (
@@ -10,7 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <>Hello World</>
+      {/* Background */}
+      <GradientBackground />
     </>
   );
 }
